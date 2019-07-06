e2f29e6f712ad716d6dbac315f39ddda6cbe2583
Kevin-Kawai <kevin.fredricks@gmail.com>
2019-07-07 03:25:18 +0900

    created subject new page

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -3,9 +3,29 @@ class SubjectsController < ApplicationController
     @subjects = Subject.where(Subject.arel_table[:name].matches("%#{params[:search_term]}%"))
   end
 
+  def new
+    @search_term = params[:search_term]
+    @subject = Subject.new(name: params[:search_term])
+  end
+
+  def create
+    subject = Subject.new(subject_params)
+    if subject.save!
+      redirect_to subject_path(subject)
+    else
+      render status: 500
+    end
+  end
+
   def show
     @subject = Subject.find_by(id: params[:id])
-    @sections = @subject.resources.first.sections
+    @sections = @subject&.resources.first&.sections
     @available_languages = @subject.available_languages
+  end
+
+  private
+
+  def subject_params
+    params.require(:subject).permit(:name)
   end
 end
