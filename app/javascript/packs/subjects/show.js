b9c0feebbd4217fac8a88c4be51a35bf0fe6e241
Kevin-Kawai <kevin.fredricks@gmail.com>
2019-07-27 22:18:40 +0900

    isolated sectionHeader rendering logic

--- app/javascript/packs/subjects/show.js
+++ app/javascript/packs/subjects/show.js
@@ -1,21 +1,15 @@
 import "./styles/show.scss"
 import axios from "axios";
 import inputField from "./inputField";
+import sectionHeader from "./sectionHeader";
 
 const addButton = document.getElementById("section-add-button");
 const deleteButtons = document.getElementsByClassName("section-delete-button");
 const sectionInputField = new inputField("section-field-inputs");
 
 const createSectionHeader = (sectionTitle, sectionId, csrfToken) => {
-  let newSectionElement = document.createElement('div')
-  newSectionElement.innerHTML = `
-    <div class="section-item">
-      <i class="material-icons section-icon__transformation">arrow_drop_down_circle</i>
-      <div>${sectionTitle}</div>
-      <i class="material-icons section-icon__transformation section-delete-button" data-delete-url="/sections/${sectionId}">clear</i>
-    </div>`
-  newSectionElement.addEventListener("click", deleteSection(csrfToken));
-  document.getElementById('section-container').appendChild(newSectionElement);
+  const sectionHeaderTemplate = new sectionHeader(sectionTitle, sectionId, deleteSection(csrfToken));
+  document.getElementById('section-container').appendChild(sectionHeaderTemplate.render());
 }
 
 const deleteSection = (csrfToken) => {
@@ -48,7 +42,6 @@ const createSection = (csrfToken) => {
           'X-CSRF-TOKEN': csrfToken
         }
       }).then((response) => {
-        console.log(response.data);
         createSectionHeader(response.data["section_title"], response.data["section_id"], csrfToken)
       }).catch((error) => {
         console.log(error);
