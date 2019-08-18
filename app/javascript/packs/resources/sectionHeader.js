339954a5591d8b7f45b3ffdfdd1964f05a5912e0
Kevin-Kawai <kevin.fredricks@gmail.com>
2019-08-18 17:03:03 +0900

    fixed delete section bug

--- app/javascript/packs/resources/sectionHeader.js
+++ app/javascript/packs/resources/sectionHeader.js
@@ -1,17 +1,20 @@
 class sectionHeader {
-  constructor(title, id, deleteCallback) {
+  constructor(resourceId, title, id, deleteCallback) {
+    this.resourceId = resourceId;
     this.title = title;
     this.id = id;
     this.callback = deleteCallback;
   }
 
+  // TODO: instead of generating the delete URL might want to get from the response and inject it
+  // this would isolate the frontend (generating the url) the backend (routing)
   renderTemplate() {
     let newSectionElement = document.createElement('div')
     newSectionElement.innerHTML = `
     <div class="section-item">
       <i class="material-icons section-icon__transformation">arrow_drop_down_circle</i>
       <div>${this.title}</div>
-      <i class="material-icons section-icon__transformation section-delete-button" data-delete-url="/sections/${this.id}">clear</i>
+      <i class="material-icons section-icon__transformation section-delete-button" data-delete-url="/resources/${this.resourceId}/sections/${this.id}">clear</i>
     </div>`
     return newSectionElement;
   }
